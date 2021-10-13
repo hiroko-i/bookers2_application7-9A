84300fd439fbf71af3e4d0b6c201aa3c7167a5b9
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-10-13 13:57:30 +0000

    [Add]4-10 User-show/edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '008c3452d28f81b5891ed159e667b952600eb28cd94a9f7699e5e19de40d906be92e6d64f54ea4188395e9efc9dead26d93bfa23f3b35f974fab2d50dee38578'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  get 'homes/new'
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about', as:'about'
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
