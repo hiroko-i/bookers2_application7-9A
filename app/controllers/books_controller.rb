62c8704fb7c2c591f36022dd676f88f04b9ef581
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-10-15 13:57:07 +0000

    [Add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,54 @@
 class BooksController < ApplicationController
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
   def show
+    # 下記のbookに対する親モデuserのデータの書き方
     @book = Book.find(params[:id])
     
   end
 
   def index
+    @user = current_user
+    @book = Book.new
     @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    # @user = User.find_by(profile_image_id: params[:profile_image_id] )
+    @book.destroy
+    redirect_to books_path
+  end
+
   
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,14 @@
 class UsersController < ApplicationController
   
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.
-    if @book.save
-    flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render "books/index"
-    end
-  end
-  
-  
   def show
     @book = Book.new
     @user = User.find(params[:id])
   end
 
   def index
-    
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def edit
