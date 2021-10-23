ecd835a5287b7743aaf818c1fab70262f46f4d7c
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-10-23 13:27:12 +0000

    [Add]いいね、コメント

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as:'about'
-  resources :books
   resources :users
+  resources :books
+    resource :favorites,only:[:create, :destroy]
+    resources :book_comments, only:[:create, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
