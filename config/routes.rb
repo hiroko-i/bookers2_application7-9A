595bc9ebb8c7c72c59a9f92eebe203fda59ff5c7
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-11-10 13:45:21 +0000

    [START]8A

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'rooms/create'
+  get 'rooms/show'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as:'about'
@@ -13,5 +15,7 @@ Rails.application.routes.draw do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
   end
+  resources :messages, only:[:create]
+  resources :rooms, only:[:create,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
